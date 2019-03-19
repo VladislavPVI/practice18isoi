867da02a3c45e745baa9effff56facbfa4818c44
Vladislav Pshenin <otori86@gmail.com>
2019-03-20 00:43:13 +0400

    + классификатор + оптимизация

--- testCam.py
+++ testCam.py
@@ -1,7 +1,11 @@
+import re
 import cv2
 import PIL.Image
 import dlib
 import numpy as np
+import os
+import pickle
+from sklearn import neighbors
 
 face_detector = dlib.get_frontal_face_detector()
 face_encoder = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
@@ -22,19 +26,6 @@ def face_locations(img):
 
     # Возвращет позицию квадрата, внутри которого находятся лица
     return [_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in _raw_face_locations(img)]
-
-def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
-
-    # Сраниваем характеристики известеных лиц и нового лица
-    # Если значение Евклидова расстояния между дескрипторами меньше 0.6,
-    # то считается, что на фотографиях один и тот же человек
-
-    if len(known_face_encodings) == 0:
-        face_distance = np.empty((0))
-    else:
-        face_distance = np.linalg.norm(known_face_encodings - face_encoding_to_check, axis=1)
-
-    return list(face_distance <= tolerance)
 
 def _css_to_rect(css):
 
@@ -71,83 +62,117 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1):
     raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model="small")
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
+def image_files_in_folder(folder):
+    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
 
-# Получаем ссылку на вебкамеру #0 (по умолчанию)
-video_capture = cv2.VideoCapture(0)
+def train(train_dir, model_save_path=None, n_neighbors=None):
 
-# Получаем характеристики лиц людей, которых необходимо распознавать
-putin_image = np.array(PIL.Image.open("putin.jpg"))
-putin_face_encoding = face_encodings(putin_image)[0]
+    # массив характеристик
+    X = []
+    # массив имен
+    y = []
 
-yakimov_image = np.array(PIL.Image.open("yakimov.jpg"))
-yakimov_face_encoding = face_encodings(yakimov_image)[0]
+    # Цикл по всем папкам тренировочного сета
+    for class_dir in os.listdir(train_dir):
+        if not os.path.isdir(os.path.join(train_dir, class_dir)):
+            continue
 
-skatova_image = np.array(PIL.Image.open("skatova.jpg"))
-skatova_face_encoding = face_encodings(skatova_image)[0]
+        # Цикл по всем изображениям папки
+        for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
+            image = np.array(PIL.Image.open(img_path))
+            face_bounding_boxes = face_locations(image)
 
-my_image = np.array(PIL.Image.open("123.jpg"))
-my_face_encoding = face_encodings(my_image)[0]
+            if len(face_bounding_boxes) != 1:
+                # Если на изображении нет лиц (или их слишком много), пропускаем данное ихображение
+                print("Изображение {} не подходит для тренинга: {}".format(img_path, "лиц нет" if len(face_bounding_boxes) < 1 else "найдено больше, чем одно лицо"))
+            else:
+                # Добавляем харк-ки найденного лица
+                X.append(face_encodings(image, known_face_locations=face_bounding_boxes)[0])
+                y.append(class_dir)
 
-lesha = np.array(PIL.Image.open("lesha.jpg"))
-lesha_face_encoding = face_encodings(lesha)[0]
+    # Определяем сколько соседей будем использовать для KNN-классификатора
+    if n_neighbors is None:
+        n_neighbors = int(round(np.sqrt(len(X))))
+        print("Число соседей выбрано автоматически:", n_neighbors)
 
-elena = np.array(PIL.Image.open("elena.jpg"))
-elena_face_encoding = face_encodings(elena)[0]
+    # Создаем и треним классификатор
+    knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm='ball_tree', weights='distance')
+    knn_clf.fit(X, y)
 
-# Создаем массивы с характеристиками и именами тех, кому они принадлежат
-known_face_encodings = [
-    putin_face_encoding,
-    yakimov_face_encoding,
-    skatova_face_encoding,
-    my_face_encoding,
-    lesha_face_encoding,
-    elena_face_encoding
-]
-known_face_names = [
-    "Vladimir Putin",
-    "Pavel Yakimov",
-    "Elena Skatova",
-    "Vladislav Pshenin",
-    "Alex Raku",
-    "Elena Makarova"
-]
+    # Сохраняем классификатор
+    if model_save_path is not None:
+        with open(model_save_path, 'wb') as f:
+            pickle.dump(knn_clf, f)
 
-while True:
-    # Извлекаем 1 кадр
-    ret, frame = video_capture.read()
+    return knn_clf
 
-    # Ковертируем изображение из BGR (используеся в OpenCV) в RGB
-    rgb_frame = frame[:, :, ::-1]
+if __name__ == "__main__":
 
-    # Находим все лица на кадре и их характеристики
-    face_locationsM = face_locations(rgb_frame)
-    face_encodingsM = face_encodings(rgb_frame, face_locationsM)
+    print("Training KNN classifier...")
+    classifier = train("knn_examples/train", model_save_path="trained_knn_model.clf", n_neighbors=2)
+    print("Training complete!")
 
-    # Цикл по всем лицам на кадре
-    for (top, right, bottom, left), face_encoding in zip(face_locationsM, face_encodingsM):
-        # Сравниваем лицо на изображении и извсетные нам лица
-        matches = compare_faces(known_face_encodings, face_encoding)
+    # Получаем ссылку на вебкамеру #0 (по умолчанию)
+    video_capture = cv2.VideoCapture(0)
 
-        name = "Unknown"
 
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
+    knn_clf = None
+    model_path="trained_knn_model.clf"
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
 
-        # Рисуем квадарат вокург лица
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    process_this_frame = True
+    names = []
+    face_locationsM = []
 
-        # Подписываем найденное лицо
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    while True:
+        # Извлекаем 1 кадр
+        ret, frame = video_capture.read()
 
-    cv2.imshow('Video', frame)
+        # Уменьшаем кадр в 4 раза для ускорения
+        little_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Нажми 'q' для выхода!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Ковертируем изображение из BGR (используеся в OpenCV) в RGB
+        little_rgb_frame = little_frame[:, :, ::-1]
 
-# Закрываем потоки вебкамеры
-video_capture.release()
-cv2.destroyAllWindows()
+        if process_this_frame:
+
+            # Находим все лица на кадре и их характеристики
+            face_locationsM = face_locations(little_rgb_frame)
+            if face_locationsM:
+                face_encodingsM = face_encodings(little_rgb_frame, face_locationsM)
+
+                closest_distances = knn_clf.kneighbors(face_encodingsM, n_neighbors=1)
+                names = []
+                faces = knn_clf.predict(face_encodingsM)
+                for i in range(len(face_locationsM)):
+                    name = "Unknown"
+                    if closest_distances[0][i][0] <= 0.6:
+                        name = faces[i]
+                    names.append(name)
+
+        process_this_frame = not process_this_frame
+        # Цикл по всем лицам на кадре
+        for (top, right, bottom, left), name in zip(face_locationsM, names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            # Рисуем квадарат вокург лица
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Подписываем найденное лицо
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
+
+        # Нажми 'q' для выхода!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Закрываем потоки вебкамеры
+    video_capture.release()
+    cv2.destroyAllWindows()
